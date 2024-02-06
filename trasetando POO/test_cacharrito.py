e3602a4563c27f5ef548507198e896866a8f2340
Yolanda Hernandez <yolanda_016@hotmail.com>
2024-02-06 22:24:22 +0100

    resolviendo errores de caligrafía en el código

--- trasetando POO/test_cacharrito.py
+++ trasetando POO/test_cacharrito.py
@@ -1,9 +1,9 @@
-from cacharritot import Cacharrito_de_Taida
+from cacharrito import Cacharrito_de_Taida
 
 def test_contador_de_Taida():
     cacharrito = Cacharrito_de_Taida()
 
-    assert cacontador == 0
+    assert contador == 0
 
     mi_funcion()
     mi_funcion()
